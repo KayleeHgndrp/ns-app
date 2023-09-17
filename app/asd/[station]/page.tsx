407661589de6e1928bd65a0ada07db8376540d79
Kaylee Hoogendorp <kaylee@deboprojects.nl>
2023-09-17 22:22:10 +0200

    add padding

--- app/asd/[station]/page.tsx
+++ app/asd/[station]/page.tsx
@@ -24,9 +24,8 @@ const StationPage = async ({ params }: { params: IParams }) => {
                 <div className="absolute bottom-20 px-20">
                     <p className="text-6xl font-bold text-white">{params.station}</p>
                 </div>
-
             </div>
-            <div className="">
+            <div className="px-10">
                 <div className="pt-16 flex flex-col gap-8">
                     <Heading title="Vertrektijden" />
                     <DepartureTable departures={vertrekTijden.departures} />
